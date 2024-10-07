4d7c1625f4542780c0744d3ed2c7656158d85edf
Aisha Maqbool <maqboolaisha7@gmail.com>
2024-10-07 00:11:30 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+var car = {
+    color: "black",
+    model: 2018,
+    price: 123400000,
+}
+var ottoman = {
+    color: "offwhite",
+    size: "medium",
+    shape: "round",
+}
+var orbitCycle = {
+    color: "black",
+    weight: 20,
+    variation: 4,
+}
+var airCondition = {
+    color: "offwhite",
+    size: "oneton",
+    shape: "rectangle",
+}
+var sofaSet = {
+    color: "brown",
+    piece: 3,
+    price: 60000,
+}
+var percianCat = {
+    color: "white",
+    age: 3,
+    breed: "boil chicken",
+}
+var novel = {
+    pages: 400,
+    size: "medium",
+    author: "umaira ahmed",
+}
+var battery = {
+    color: "white,red",
+    ampere: 100,
+    weightinkg: 30,
+}
+var vaccumFlask = {
+    color: "grey",
+    capacityinlitre: 1,
+    priceinrupees: 1800,
+}
+var electricKettle = {
+    metal: "silver",
+    capacityinlitre: 2,
+    priceinrupees: 5000,
+}
+
+
+
+
+
+console.log(car, ottoman, percianCat, sofaSet, airCondition, orbitCycle, novel, battery, vaccumFlask, electricKettle)
